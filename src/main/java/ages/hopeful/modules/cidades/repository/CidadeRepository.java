a869ff07ff34c7877020bb3019d6ee0da48c3862
Andre Dame Fabiao <andre.fabiao@outlook.com>
2025-08-28 14:50:34 -0300

    add base test examples

---
+++ src/main/java/ages/hopeful/modules/cidades/repository/CidadeRepository.java
@@ -0,0 +1,13 @@
+package ages.hopeful.modules.cidades.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ages.hopeful.modules.cidades.model.Cidade;
+
+import java.util.UUID;
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, UUID> {
+}
