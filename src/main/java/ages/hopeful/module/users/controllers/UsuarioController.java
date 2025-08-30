a5b64b4883b9b2a866fe672f3aa8e35b3a3026c5
NicolasTondo01 <nicolas.tondo@edu.pucrs.br>
2025-08-30 13:58:07 -0300

    Nicolas e Eduardo: Criação do Controller para user

---
+++ src/main/java/ages/hopeful/module/users/controllers/UsuarioController.java
@@ -0,0 +1,14 @@
+package ages.hopeful.module.users.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios") 
+public class UsuarioController {
+
+    
+}
