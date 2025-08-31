cf9f74b2608f3f0502d2b86c9633200badb512e8
Andre Dame Fabiao <andre.fabiao@outlook.com>
2025-08-31 15:11:08 -0300

    fix path exception

---
+++ src/main/java/ages/hopeful/common/exception/HttpException.java
@@ -0,0 +1,20 @@
+package ages.hopeful.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class HttpException extends RuntimeException {
+
+  private final HttpStatus status;
+
+  public HttpException(final String message, final HttpStatus httpStatus) {
+    super(message);
+    this.status = httpStatus;
+  }
+
+  public HttpException(final String message) {
+    super(message);
+    this.status = HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+}
