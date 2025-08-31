0252a67d1c4f82911ece30540bab949c55e57923
Andre Dame Fabiao <andre.fabiao@outlook.com>
2025-08-31 13:07:08 -0300

    fix path files

--- src/main/java/ages/hopeful/shared/exception/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package ages.hopeful.shared.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class NotFoundException extends HttpException {
-  public NotFoundException(String message) {
-    super(message, HttpStatus.NOT_FOUND);
-  }
-}
