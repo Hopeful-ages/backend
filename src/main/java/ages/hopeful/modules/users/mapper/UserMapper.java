646ce4d4bea79064fc311cec842b9d246ddc74e6
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-09-01 18:30:46 -0300

    Corrections

--- src/main/java/ages/hopeful/modules/users/mapper/UserMapper.java
+++ src/main/java/ages/hopeful/modules/users/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import ages.hopeful.modules.users.dto.*;
 import ages.hopeful.modules.users.model.User;
 
 public class UserMapper {
+
     public static User toEntity(UserCreateDTO dto, String passwordHash) {
         User entity = new User();
 
@@ -11,8 +12,8 @@ public class UserMapper {
         entity.setCpf(dto.getCpf().replaceAll("[^0-9]", ""));
         entity.setEmail(dto.getEmail().toLowerCase());
         entity.setPhone(dto.getPhone());
-        entity.setPasswordHash(passwordHash);
-        entity.setServiceId(dto.getServicoId());
+        entity.setPassword(dto.getPassword());
+        entity.setServiceId(dto.getServiceId());
         entity.setCityId(dto.getCityId());
 
         return entity;
@@ -20,24 +21,24 @@ public class UserMapper {
 
     public static UserResponseDTO toResponse(User entity) {
         return UserResponseDTO.builder()
-                .id(entity.getId())
-                .name(entity.getName())
-                .cpf(entity.getCpf())
-                .email(entity.getEmail())
-                .phone(entity.getPhone())
-                .serviceId(entity.getServiceId())
-                .cityId(entity.getCityId())
-                .build();
+            .id(entity.getId())
+            .name(entity.getName())
+            .cpf(entity.getCpf())
+            .email(entity.getEmail())
+            .phone(entity.getPhone())
+            .serviceId(entity.getServiceId())
+            .cityId(entity.getCityId())
+            .build();
     }
 
-    public static void updateEntity (User entity, UserUpdateDTO userUpdateDTO) {
-        entity.setName(userUpdateDTO.name.trim());
-        entity.setCpf(userUpdateDTO.cpf.replaceAll("[^0-9]", ""));
-        entity.setEmail(userUpdateDTO.email.toLowerCase());
-        entity.setPasswordHash(encoder.encode(userUpdateDTO.password));
-        entity.setPhone(userUpdateDTO.phone);
-        entity.service(userUpdateDTO.service);
-        entity.setCity(userUpdateDTO.city);
+    public static void updateEntity(User entity, UserUpdateDTO dto) {
+        entity.setName(dto.getName().trim());
+        entity.setCpf(dto.getCpf().replaceAll("[^0-9]", ""));
+        entity.setEmail(dto.getEmail().toLowerCase());
+        entity.setPassword(dto.getPassword());
+        //Proteger password com hash
+        entity.setPhone(dto.getPhone());
+        entity.service(dto.getServiceId());
+        entity.setCity(dto.getCityId());
     }
 }
-
