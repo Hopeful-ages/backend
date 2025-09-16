23d8f8a0ab7220a6c9d7cb08df05eca4fdcb5b64
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2025-09-16 20:14:27 -0300

    .

--- src/main/resources/db/migration/V6__add_enum_to_criticalities.sql
+++ src/main/resources/db/migration/V6__add_enum_to_criticalities.sql
@@ -1,7 +1,1 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'criticality_phase') THEN
-        CREATE TYPE criticality_phase AS ENUM ('antes', 'durante', 'depois');
-    END IF;
-END
-$$;
+CREATE TYPE criticality_phase AS ENUM ('antes', 'durante', 'depois');
