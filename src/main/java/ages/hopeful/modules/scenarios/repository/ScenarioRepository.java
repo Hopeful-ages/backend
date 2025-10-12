91b55531912ba67d0f3954c9d64356c8dd5cb9c9
pedrodalagnol <joao.rossatti@edu.pucrs.br>
2025-10-12 14:14:05 -0300

    (Colaboradores: Ravel e João Pedro) Desenvolvimento da rota para buscar cenários published de uma cidade, cobrade específica, ou nenhum dos dois. Torna-la de acesso público e implementação de endpoint temporário para tornar um cenário published.

--- src/main/java/ages/hopeful/modules/scenarios/repository/ScenarioRepository.java
+++ src/main/java/ages/hopeful/modules/scenarios/repository/ScenarioRepository.java
@@ -2,12 +2,26 @@ package ages.hopeful.modules.scenarios.repository;
 
 import ages.hopeful.modules.scenarios.model.Scenario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ScenarioRepository extends JpaRepository<Scenario, UUID> {
     Optional<Scenario> findByCobradeIdAndCityId(UUID id, UUID id1);
+
+    @Query("""
+        SELECT s FROM Scenario s
+        WHERE s.published = true
+        AND (:cobradeId IS NULL OR s.cobrade.id = :cobradeId)
+        AND (:cityId IS NULL OR s.city.id = :cityId)
+    """)
+    List<Scenario> findByCobradeIdAndCityIdSearch(
+            @Param("cityId") UUID cityId,
+            @Param("cobradeId") UUID cobradeId
+    ) ;
 }
