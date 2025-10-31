90c9cbedf58a2c4eb0f74d57ec9a137328cdc1b2
Rodrigo Gagliardi <rodrigogagliardi18@gmail.com>
2025-10-30 21:10:17 -0300

    não há mais erros de compilação, porém ainda não tenho certeza se está correto

--- src/test/java/ages/hopeful/modules/scenario/service/ScenarioServiceTest.java
+++ src/test/java/ages/hopeful/modules/scenario/service/ScenarioServiceTest.java
@@ -1,13 +1,31 @@
 package ages.hopeful.modules.scenario.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 
 import ages.hopeful.common.exception.NotFoundException;
 import ages.hopeful.modules.city.model.City;
 import ages.hopeful.modules.cobrades.model.Cobrade;
-import ages.hopeful.modules.services.model.Service;
 import ages.hopeful.modules.scenarios.dto.ParameterRequestDTO;
 import ages.hopeful.modules.scenarios.dto.ScenarioRequestDTO;
 import ages.hopeful.modules.scenarios.dto.ScenarioResponseDTO;
@@ -15,15 +33,7 @@ import ages.hopeful.modules.scenarios.dto.TaskRequestDTO;
 import ages.hopeful.modules.scenarios.model.Scenario;
 import ages.hopeful.modules.scenarios.repository.ScenarioRepository;
 import ages.hopeful.modules.scenarios.service.ScenarioService;
-import java.util.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
+import ages.hopeful.modules.services.model.Service;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Unit tests for ScenarioService")
@@ -83,12 +93,12 @@ class ScenarioServiceTest {
                 .phase("Fase")
                 .build();
 
-        taskRequestDTO = TaskRequestDTO.builder()
+        
+            taskRequestDTO = TaskRequestDTO.builder()
                 .description("Descrição")
                 .phase("Fase")
                 .lastUpdateDate(new Date(System.currentTimeMillis()))
-                .service(service)
-                .scenario(scenario)
+                .serviceId(serviceId)
                 .build();
 
         scenarioRequestDTO = ScenarioRequestDTO.builder()
