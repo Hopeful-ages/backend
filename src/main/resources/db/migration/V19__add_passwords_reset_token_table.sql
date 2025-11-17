0b1fb79e80041150f0442e29ac2dfd4a3d649d92
Lucca Zen Mazzoccato <luccazenm@icloud.com>
2025-11-16 23:15:14 -0300

    migration change

--- src/main/resources/db/migration/V19__add_passwords_reset_token_table.sql
+++ src/main/resources/db/migration/V19__add_passwords_reset_token_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE password_reset_tokens (
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    expires_at TIMESTAMP NOT NULL,
+CREATE TABLE IF NOT EXISTS password_reset_tokens (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
     token VARCHAR(255) NOT NULL UNIQUE,
-    used BOOLEAN DEFAULT FALSE,
-    user_id UUID REFERENCES users(id)
+    expires_at TIMESTAMP NOT NULL,
+    used BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
 );
